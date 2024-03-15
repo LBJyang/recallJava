fdf7b33e20e5a57b35d795ff2045a8100b81d132
lbjyangoffice <yangfan6262008@gmail.com>
2024-03-15 16:11:14 +0800

    IOC review done

--- spring-ioc-resource/src/main/java/com/itranswarp/learnjava/AppService.java
+++ spring-ioc-resource/src/main/java/com/itranswarp/learnjava/AppService.java
@@ -1,9 +1,14 @@
 package com.itranswarp.learnjava;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -18,10 +23,13 @@ public class AppService {
 	@Value("1")
 	private int version;
 
-	@Value("classpath:src/main/resources/logo.txt")
+	@Value("classpath:/logo.txt")
 	private Resource resource;
+	@Value("classpath:/app.properties")
+	private Resource resourcePropResource;
 
 	private String logo;
+	private String prop;
 
 	@PostConstruct
 	public void init() throws IOException {
@@ -29,10 +37,16 @@ public class AppService {
 				new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8))) {
 			this.logo = reader.lines().collect(Collectors.joining("\n"));
 		}
+		try (var reader = new BufferedReader(
+				new InputStreamReader(resourcePropResource.getInputStream(), StandardCharsets.UTF_8))) {
+			this.prop = reader.lines().collect(Collectors.joining("\n"));
+		}
+
 	}
 
 	public void printLogo() {
 		System.out.println(logo);
+		System.out.println(prop);
 		System.out.println("app.version: " + version);
 	}
 }
