9a626623f934ae1535204989a9be3943a11b8e58
lbjyangofficenew <yangfan6262008@gmail.com>
2024-05-22 17:14:23 +0800

    java recall almost done

--- springbootplus/src/main/java/Hongze/springbootplus/Application.java
+++ springbootplus/src/main/java/Hongze/springbootplus/Application.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
